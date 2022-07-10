37ff74818f4bbeca05ea8618fd628d48302dbc30
ZeroKun265 <davidegiambirtone265@gmail.com>
2022-07-10 17:09:29 +0200

    Added ModBlocks class

---
+++ src/main/java/zerokun265/fabric/copperrings/block/ModBlocks.java
@@ -0,0 +1,10 @@
+package zerokun265.fabric.copperrings.block;
+
+import zerokun265.fabric.copperrings.CopperRings;
+
+public class ModBlocks {
+
+    public static void registerModBlocks(){
+        CopperRings.LOGGER.info("Registering ModBlocks for mod: " + CopperRings.MODID);
+    }
+}
